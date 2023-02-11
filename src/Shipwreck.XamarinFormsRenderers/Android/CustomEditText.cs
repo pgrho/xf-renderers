c25af1bd65c0411d01591e0c47cdae8a63e9586d
pgrho <shipwreck@live.jp>
2023-02-11 11:02:17 +0900

    Changed FormsEditText to CustomEditText

---
+++ src/Shipwreck.XamarinFormsRenderers/Android/CustomEditText.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using Android.Content;
+using Android.Views;
+using Android.Widget;
+using AndroidX.Core.Graphics.Drawable;
+using Xamarin.Forms.Platform.Android;
+using ARect = Android.Graphics.Rect;
+
+namespace Shipwreck.XamarinFormsRenderers.Android;
+
+public class CustomEditText : FormsEditTextBase
+{
+    public CustomEditText(Context context)
+        : base(context)
+    {
+    }
+
+    public override bool OnKeyPreIme(Keycode keyCode, KeyEvent e)
+    {
+        if (keyCode != Keycode.Back || e.Action != KeyEventActions.Down)
+        {
+            return base.OnKeyPreIme(keyCode, e);
+        }
+
+        this.HideKeyboard();
+
+        OnKeyboardBackPressed?.Invoke(this, EventArgs.Empty);
+        return true;
+    }
+
+    protected override void OnSelectionChanged(int selStart, int selEnd)
+    {
+        base.OnSelectionChanged(selStart, selEnd);
+        SelectionChanged?.Invoke(this, new(selStart, selEnd));
+    }
+
+    internal event EventHandler OnKeyboardBackPressed;
+
+    internal event EventHandler<Xamarin.Forms.Platform.Android.SelectionChangedEventArgs> SelectionChanged;
+}
--- src/Shipwreck.XamarinFormsRenderers/Android/TextColorSwitcher.cs
+++ src/Shipwreck.XamarinFormsRenderers/Android/TextColorSwitcher.cs
@@ -2,55 +2,54 @@
 using Android.Content.Res;
 using Android.Widget;
 
-namespace Xamarin.Forms.Platform.Android
+namespace Shipwreck.XamarinFormsRenderers.Android;
+
+/// <summary>
+/// Handles color state management for the TextColor property 
+/// for Entry, Button, Picker, TimePicker, and DatePicker
+/// </summary>
+internal class TextColorSwitcher
 {
-    /// <summary>
-    /// Handles color state management for the TextColor property 
-    /// for Entry, Button, Picker, TimePicker, and DatePicker
-    /// </summary>
-    internal class TextColorSwitcher
+    static readonly int[][] s_colorStates = { new[] { global::Android.Resource.Attribute.StateEnabled }, new[] { -global::Android.Resource.Attribute.StateEnabled } };
+
+    readonly ColorStateList _defaultTextColors;
+    readonly bool _useLegacyColorManagement;
+    Color _currentTextColor;
+
+    public TextColorSwitcher(ColorStateList textColors, bool useLegacyColorManagement = true)
     {
-        static readonly int[][] s_colorStates = { new[] { global::Android.Resource.Attribute.StateEnabled }, new[] { -global::Android.Resource.Attribute.StateEnabled } };
+        _defaultTextColors = textColors;
+        _useLegacyColorManagement = useLegacyColorManagement;
+    }
 
-        readonly ColorStateList _defaultTextColors;
-        readonly bool _useLegacyColorManagement;
-        Color _currentTextColor;
+    public void UpdateTextColor(TextView control, Color color, Action<ColorStateList> setColor = null)
+    {
+        if (color == _currentTextColor)
+            return;
 
-        public TextColorSwitcher(ColorStateList textColors, bool useLegacyColorManagement = true)
+        if (setColor == null)
         {
-            _defaultTextColors = textColors;
-            _useLegacyColorManagement = useLegacyColorManagement;
+            setColor = control.SetTextColor;
         }
 
-        public void UpdateTextColor(TextView control, Color color, Action<ColorStateList> setColor = null)
+        _currentTextColor = color;
+
+        if (color.IsDefault)
         {
-            if (color == _currentTextColor)
-                return;
-
-            if (setColor == null)
+            setColor(_defaultTextColors);
+        }
+        else
+        {
+            if (_useLegacyColorManagement)
             {
-                setColor = control.SetTextColor;
-            }
-
-            _currentTextColor = color;
-
-            if (color.IsDefault)
-            {
-                setColor(_defaultTextColors);
+                // Set the new enabled state color, preserving the default disabled state color
+                int defaultDisabledColor = _defaultTextColors.GetColorForState(s_colorStates[1], color.ToAndroid());
+                setColor(new ColorStateList(s_colorStates, new[] { color.ToAndroid().ToArgb(), defaultDisabledColor }));
             }
             else
             {
-                if (_useLegacyColorManagement)
-                {
-                    // Set the new enabled state color, preserving the default disabled state color
-                    int defaultDisabledColor = _defaultTextColors.GetColorForState(s_colorStates[1], color.ToAndroid());
-                    setColor(new ColorStateList(s_colorStates, new[] { color.ToAndroid().ToArgb(), defaultDisabledColor }));
-                }
-                else
-                {
-                    var acolor = color.ToAndroid().ToArgb();
-                    setColor(new ColorStateList(s_colorStates, new[] { acolor, acolor }));
-                }
+                var acolor = color.ToAndroid().ToArgb();
+                setColor(new ColorStateList(s_colorStates, new[] { acolor, acolor }));
             }
         }
     }
