c03e6a26bd8d61eb2fff0159dabfdfd23b48afad
pgrho <shipwreck@live.jp>
2023-02-11 21:22:37 +0900

    Added ExtendedEntry

---
+++ src/Shipwreck.XamarinFormsRenderers/Android/ExtendedEntryRenderer.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using Android.Content;
+using Shipwreck.XamarinFormsRenderers;
+using Shipwreck.XamarinFormsRenderers.Android;
+
+[assembly: ExportRenderer(typeof(ExtendedEntry), typeof(ExtendedEntryRenderer))]
+namespace Shipwreck.XamarinFormsRenderers.Android;
+
+public class ExtendedEntryRenderer : EntryRenderer
+{
+    public ExtendedEntryRenderer(Context context)
+        : base(context)
+    {
+    }
+
+    public new ExtendedEntry Element => (ExtendedEntry)base.Element;
+
+    protected override void OnElementChanged(Xamarin.Forms.Platform.Android.ElementChangedEventArgs<Entry> e)
+    {
+        base.OnElementChanged(e);
+
+        UpdateSelectAllOnFocus();
+    }
+
+    protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        switch (e.PropertyName)
+        {
+            case nameof(Element.SelectAllOnFocus):
+                UpdateSelectAllOnFocus();
+                break;
+        }
+
+        base.OnElementPropertyChanged(sender, e);
+    }
+
+    protected virtual void UpdateSelectAllOnFocus()
+    {
+        if (Element is ExtendedEntry e && Control is CustomEditText c)
+        {
+            c.SetSelectAllOnFocus(e.SelectAllOnFocus);
+        }
+    }
+}
---
+++ src/Shipwreck.XamarinFormsRenderers/ExtendedEntry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipwreck.XamarinFormsRenderers;
+
+public class ExtendedEntry : Entry
+{
+    #region SelectAllOnFocus
+
+    public static readonly BindableProperty SelectAllOnFocusProperty
+        = BindableProperty.Create(
+            nameof(SelectAllOnFocus), typeof(bool), typeof(ExtendedEntry),
+            defaultValue: true);
+
+    public bool SelectAllOnFocus
+    {
+        get => (bool)GetValue(SelectAllOnFocusProperty);
+        set => SetValue(SelectAllOnFocusProperty, value);
+    }
+
+    #endregion SelectAllOnFocus
+
+}
