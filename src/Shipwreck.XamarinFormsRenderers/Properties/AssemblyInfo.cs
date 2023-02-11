d1829bb7d233a73e5ef3a933a98e16781e1850f5
pgrho <shipwreck@live.jp>
2023-02-11 22:23:31 +0900

    Added IsTrimmable

--- src/Shipwreck.XamarinFormsRenderers/Properties/AssemblyInfo.cs
+++ src/Shipwreck.XamarinFormsRenderers/Properties/AssemblyInfo.cs
@@ -3,6 +3,8 @@ global using System.Collections.Generic;
 global using System.Text;
 global using Xamarin.Forms;
 global using Xamarin.Forms.Internals;
+using System.Reflection;
 
 [assembly: XmlnsDefinition("http://schema.shipwreck.jp/xf-renderers/", "Shipwreck.XamarinFormsRenderers")]
 [assembly: XmlnsPrefix("http://schema.shipwreck.jp/xf-renderers/", "xfr")]
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
